3918cb399bf613d81faa8befdd997da4acdeb927
Luis Felipe <luisfelipe1582@hotmail.com>
2022-06-10 01:38:25 -0300

    atualizado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+livros = Livro.create([{ titulo: "Homem de Giz"}, {autor: "C.J Tudor"}, {ano: "2010"}])
